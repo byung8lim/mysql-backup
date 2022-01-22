e429f13b4fc9147579e0cefb6cf8f5f3ad2f22af
Byung Ho Lim <40503142+byung8lim@users.noreply.github.com>
2022-01-23 08:30:08 +0900

    Create list.sh
    list backups

---
+++ backup-agent/scripts/list.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
+BASE="/byung8"
+HOME="/mbs"
+CONF="$HOME/conf/mbs.conf"
+OBJ_STORAGE_URL=$(grep 'OBJ_STORAGE_URL' $CONF | awk -F '=' '{print $2}')
+BUCKET_NAME=$(grep 'BUCKET_NAME' $CONF | awk -F '=' '{print $2}')
+BACKUP_PREFIX=$(grep 'BACKUP_PREFIX' $CONF | awk -F '=' '{print $2}')
+LOGFILE="$HOME/logs/list-$(date +%Y%m%d).log"
+
+function log() {
+  echo "[$(date +%F.%T.%3N)][INFO] $*" >> $LOGFILE
+}
+
+function warn() {
+  echo "[$(date +%F.%T.%3N)][WARN] $*" >> $LOGFILE
+}
+
+function error() {
+  echo "[$(date +%F.%T.%3N)][ERROR] $*" >> $LOGFILE
+}
+
+echo "OBJ_STORAGE_URL: $OBJ_STORAGE_URL"
+echo "BUCKET_NAME: $BUCKET_NAME"
+echo "NAMESPACE: $NAMESPACE"
+
+echo "aws --endpoint-url=$OBJ_STORAGE_URL s3 ls s3://$BUCKET_NAME/$NAMESPACE/backups/"
+aws --endpoint-url=$OBJ_STORAGE_URL s3 ls s3://$BUCKET_NAME/$NAMESPACE/backups/
