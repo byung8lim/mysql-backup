699b80a6b8928703ada91f1e99bc235724f88693
Byung Ho Lim <40503142+byung8lim@users.noreply.github.com>
2022-01-24 08:41:53 +0900

    Update 00_create_scheme_account.sh
    11111111111

--- mysql2/init_script/00_create_scheme_account.sh
+++ mysql2/init_script/00_create_scheme_account.sh
@@ -1,3 +1,2 @@
-create database fitness;
-create user byung8@'%' identified by '1q2w3e4r5t';
+create user byung8@'%' identified by '1q2w3e4r5t' PASSWORD EXPIRE NEVER;
 grant all privileges on fitness.* to byung8@'%' with grant option;
